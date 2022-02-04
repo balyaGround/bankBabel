46dfba79869f01e269a0b4548f38dbac4ed2ddd6
balyaGround <balyazulfikar@gmail.com>
2022-02-04 18:19:37 +0700

    ready for finishing

--- src/component/scheduling.jsx
+++ src/component/scheduling.jsx
@@ -6,41 +6,36 @@ import "firebase/compat/firestore";
 import config from "../config";
 function Schedulling() {
   firebase.initializeApp(config);
-  const db = firebase.firestore();
-  const [schedule, setSchedule] = useState({
-    date: "",
-    email: "",
-    nik: "",
-    time: "",
-  });
-  async getMarkers() {
-    const events = await firebase.firestore().collection('schedule')
-    events.get().then((querySnapshot) => {
-        const tempDoc = querySnapshot.docs.map((doc) => {
-          return { id: doc.id, ...doc.data() }
-        })
-        console.log(tempDoc)
-      })
-  }
+  const [data, setData] = useState([]);
   const getSchedule = () => {
-    db.collection("schedule")
-      .orderBy("date", "asc")
-      .get()
-      .then((doc) => {
-        doc.forEach((doc) => {
-          const jsonData = doc.data();
-          const jsonString = JSON.stringify(jsonData);
-          console.log("json", jsonString);
-          const json = JSON.parse(jsonString);
-          setSchedule({ ...schedule, date: json.date, email: json.email, nik: json.nik, time: json.time });
-        });
+    const events = firebase.firestore().collection("schedule").orderBy("date", "asc");
+    events.get().then((querySnapshot) => {
+      const tempDoc = querySnapshot.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() };
       });
+      console.log(tempDoc);
+      setData(tempDoc);
+    });
   };
+  // const getSchedule = () => {
+  //   db.collection("schedule")
+  //     .orderBy("date", "asc")
+  //     .get()
+  //     .then((doc) => {
+  //       doc.forEach((doc) => {
+  //         const jsonData = doc.data();
+  //         const jsonString = JSON.stringify(jsonData);
+  //         console.log("json", jsonString);
+  //         const json = JSON.parse(jsonString);
+  //         setSchedule({ ...schedule, date: json.date, email: json.email, nik: json.nik, time: json.time });
+  //       });
+  //     });
+  // };
 
   useEffect(() => {
     getSchedule();
   }, []);
-  console.log("schedule", schedule);
+  console.log("data", data);
   return (
     <>
       <div className="schedule">
@@ -52,7 +47,7 @@ function Schedulling() {
             <div class="container album-card">
               <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                 <div class="col">
-                  {schedule.map((item) => (
+                  {data.map((item) => (
                     <div class="card shadow-sm">
                       <div class="card-body">
                         <p class="card-text">New Request !!</p>
@@ -66,6 +61,9 @@ function Schedulling() {
                             </button>
                             <button type="button" class="btn btn-sm btn-outline-secondary">
                               Decline
+                            </button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary">
+                              Make room
                             </button>
                           </div>
                           <small class="text-muted">{item.date}</small>
