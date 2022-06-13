b9d411cd8534d473d1b00d5ed2d3636b8c60ab20
balyaGround <balyazulfikar@gmail.com>
2022-06-14 06:47:46 +0700

    settimer push notip

--- src/component/shedulevideo.jsx
+++ src/component/shedulevideo.jsx
@@ -63,14 +63,15 @@ function Schedulevideo() {
       },
       data: data,
     };
-
-    await axios(config)
-      .then((response) => {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    setTimeout(() => {
+      axios(config)
+        .then((response) => {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 1500);
   };
   useEffect(() => {
     getDataParameter();
